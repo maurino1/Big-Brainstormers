008ba1b134a1c758ea186f2f7f510775b031d622
MuMolters <n.jansen12@student.avans.nl>
2025-03-21 13:16:32 +0100

    AvatarScene

---
+++ Assets/Scripts/AvatarScene.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class AvatarScene : MonoBehaviour
+{
+    public Button profileButton;
+    public Button FAQButton;
+    public Button avatarButton;
+
+    private void Start()
+    {
+        profileButton.onClick.AddListener(() => SceneManager.LoadScene("Login"));
+        FAQButton.onClick.AddListener(() => SceneManager.LoadScene("FAQ"));
+        avatarButton.onClick.AddListener(() => SceneManager.LoadScene("CharacterSelectScreen"));
+    }
+}
