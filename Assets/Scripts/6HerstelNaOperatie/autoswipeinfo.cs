22a211577027ceb8e7ddd7b42b220ee8c5368f28
maurino1 <jajagta7@gmai.com>
2025-03-31 23:31:00 +0200

    scene 7 swipe functie voor info

---
+++ Assets/Scripts/6HerstelNaOperatie/autoswipeinfo.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AutoSwipeInfo : MonoBehaviour
+{
+    public ScrollRect scrollRect;
+    public float swipeInterval = 3f;
+    public float swipeSpeed = 0.5f;
+
+    public Button nextButton;
+    public Button prevButton;
+
+    private int totalPages;
+    private int currentPage = 0;
+    private float[] positions;
+    private Coroutine swipeCoroutine;
+
+    void Start()
+    {
+        totalPages = scrollRect.content.childCount;
+        positions = new float[totalPages];
+
+        for (int i = 0; i < totalPages; i++)
+        {
+            positions[i] = (float)i / (totalPages - 1);
+        }
+
+        nextButton.onClick.AddListener(NextPage);
+        prevButton.onClick.AddListener(PreviousPage);
+
+        swipeCoroutine = StartCoroutine(AutoSwipe());
+    }
+
+    IEnumerator AutoSwipe()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(swipeInterval);
+            NextPage();
+        }
+    }
+
+    void NextPage()
+    {
+        if (swipeCoroutine != null) StopCoroutine(swipeCoroutine);
+
+        currentPage = (currentPage + 1) % totalPages;
+        swipeCoroutine = StartCoroutine(SmoothSwipe(positions[currentPage]));
+        swipeCoroutine = StartCoroutine(AutoSwipe());
+    }
+
+    void PreviousPage()
+    {
+        if (swipeCoroutine != null) StopCoroutine(swipeCoroutine);
+
+        currentPage = (currentPage - 1 + totalPages) % totalPages;
+        swipeCoroutine = StartCoroutine(SmoothSwipe(positions[currentPage]));
+        swipeCoroutine = StartCoroutine(AutoSwipe());
+    }
+
+    IEnumerator SmoothSwipe(float targetPosition)
+    {
+        float start = scrollRect.horizontalNormalizedPosition;
+        float time = 0f;
+
+        while (time < swipeSpeed)
+        {
+            time += Time.deltaTime;
+            scrollRect.horizontalNormalizedPosition = Mathf.Lerp(start, targetPosition, time / swipeSpeed);
+            yield return null;
+        }
+
+        scrollRect.horizontalNormalizedPosition = targetPosition;
+    }
+}
